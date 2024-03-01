ef5ab9d5dbb47b923e8b1ac0470b8490fcb2b505
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2024-03-01 15:25:22 +0000

    Added appropiate elements accordance to design
    Added bc, input boxes, title for modules, some css changes.

--- main.js
+++ main.js
@@ -5,15 +5,29 @@
 
 document.addEventListener('DOMContentLoaded', function() { // Event listenr for the DOM
     var addButton = document.querySelector('.modules button');
-    var container = document.querySelector('.modules');
-
+    var addModulesContainer = document.querySelector(".add-modules")
+    // Adding the module
     addButton.addEventListener('click', function() { // Event listener for the + add more button, to add modules.
         var html = buttonClicked();
-        container.insertAdjacentHTML('beforebegin', html);
+        addModulesContainer.insertAdjacentHTML('beforebegin', html);
     })
 })
 
 function buttonClicked() // + add more button function when it is being clicked on by a user.
 {
-    return `<div> test </div>`;
+    return`
+    <div class="module">
+        <div class="module-row">
+            <div class="module-name">
+                <input type="text" placeholder="New Module">
+            </div>
+            <div class="module-grade-current">
+                <input type="number" placeholder="current" min="0" max="100">
+            </div>  
+            <div class="module-grade-whatif">
+                <input type="number" placeholder="what if" min="0" max="100">
+            </div>
+        </div>
+    </div>
+    `;
 }
