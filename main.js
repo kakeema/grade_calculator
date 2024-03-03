f0035ce41de93779341929cb23c64275c8982772
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2024-03-03 20:38:52 +0000

    Can now calculate the average of the grades
    Removed the alert message in html for this button, and only added the calculation as an alert.

--- main.js
+++ main.js
@@ -5,12 +5,19 @@
 
 document.addEventListener('DOMContentLoaded', function() { // Event listenr for the DOM
     var addButton = document.querySelector('.modules button');
-    var addModulesContainer = document.querySelector(".add-modules")
+    var addModulesContainer = document.querySelector(".add-modules");
+    var calculateButton = document.querySelector('.calculate-button button');
+
     // Adding the module
     addButton.addEventListener('click', function() { // Event listener for the + add more button, to add modules.
         var html = buttonClicked();
         addModulesContainer.insertAdjacentHTML('beforebegin', html);
     })
+
+    // Calculate button
+    calculateButton.addEventListener('click', function() {
+        calculateAverage();
+    });
 })
 
 function buttonClicked() // + add more button function when it is being clicked on by a user.
@@ -30,4 +37,39 @@ function buttonClicked() // + add more button function when it is being clicked 
         </div>
     </div>
     `;
+}
+
+function calculateAverage() {
+    var currentGrades = document.querySelectorAll('.module-grade-current input');
+    var grades = [];
+
+    // Collect all current grades into an array.
+    currentGrades.forEach(function(input) {
+        var grade = parseInt(input.value, 10); // Parse the value as a base-10 integer.
+        if (!isNaN(grade)) { // Check if the parsed value is a number.
+            grades.push(grade);
+        }
+    });
+
+    // Sorting the grades to find the smallest result.
+    grades.sort(function(a, b) {
+        return a - b;
+    });
+
+    // Check if there are at least four modules. NOTE: I do not know if 4 is the minimum or not, if we are allowed to have 1 single module I will have to change things around for this calculation. 
+    if (grades.length < 4) {
+        alert('Please enter at least four modules.');
+        return;
+    }
+
+    // Calculate total value considering half the smallest grade and full of the others.
+    var totalValue = (grades[0] / 2) + grades.slice(1).reduce(function(acc, grade) {
+        return acc + grade;
+    }, 0);
+
+    // Calculate the average.
+    var average = totalValue / 3.5;
+
+    // Display the result.
+    alert('The average grade is: ' + average.toFixed(2));
 }
