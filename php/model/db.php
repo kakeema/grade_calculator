e48c3f91258860532b553486fff68fce3bef4359
Mustaqeem <Mustaqeem.A.Chowdhury@gmail.com>
2024-03-04 15:32:54 +0000

    attempting to fix bugs

--- php/model/db.php
+++ php/model/db.php
@@ -1,6 +1,12 @@
 <?php
-$pdo = new PDO("mysql:host=localhost;dbname=db_k2230216", "k2230216", "ephahpae", 
-[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]); 
+$serverName = "localhost";
+$database = "db_k2230216";
+$username = "k2230216";
+$password = "ephahpae";
+
+$pdo = new PDO("mysql:host=$serverName;dbname=$database", $username, $password, 
+[PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    
 // The above is connecting to the database, we specfy the database name, and then we have to give it the username and the password to get into that database.
 // The PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION are error messages, and are not actually needed to connect to the database.
 // Try and connect using the info above.
